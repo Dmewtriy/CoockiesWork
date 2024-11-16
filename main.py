a06a49cc57908336f5821fa4eea3437c1e0d18aa
Dmewtriy <dankin.dm@mail.ru>
2024-11-16 22:22:36 +0700

    изменена бд, готовый код для snapshot, из бд удалена информация о квартирах, оказалось что она бесполезна

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-﻿from tkinter import TRUE
+﻿from re import I
+from tkinter import TRUE
 import telebot
 import requests
 import json
@@ -31,6 +32,10 @@ def start(message):
 @bot.message_handler(content_types=['contact'])
 def handle_contact(message):
     phone_number = message.contact.phone_number
+    user_already_exist = session.query(models.User).filter_by(phone_number=phone_number).first()
+    if user_already_exist is not None:
+        bot.send_message(message.from_user.id, 'Вы уже зарегистрированы.')
+        return
     tenant_id = 1064 #get_tenant_id(phone_number) 
     if tenant_id is None:
         bot.send_message(message.chat.id, 'Ошибка авторизации. Введите корректный номер телефона.')
@@ -43,9 +48,9 @@ def handle_contact(message):
     bot.send_message(message.chat.id, 'Вы успешно авторизованы!')
 
     # Получаем квартиры и домофоны
-    add_apartments_and_domofons(user)
+    add_domofons(user)
 
-def add_apartments_and_domofons(user):
+def add_domofons(user):
     tenant_id = user.tenant_id
 
     # Запрос квартир
@@ -55,28 +60,23 @@ def add_apartments_and_domofons(user):
         apartments = response_apartments.json()
         if apartments:
             for apartment in apartments:
-                # Добавляем квартиру в БД
-                apartment_record = models.Apartment(user_id=user.id, apartment_number=apartment['id'])
-                session.add(apartment_record)
-
                 # Запрос домофонов для квартиры
                 response_domofons = requests.get(f'{DOMOFON_API_URL}domo.apartment/{apartment["id"]}/domofon?tenant_id={tenant_id}', headers=headers)
                 if response_domofons.status_code == 200:
                     domofons = response_domofons.json()
                     for domofon in domofons:
                         # Добавляем домофон в БД
-                        domofon_record = models.Domofon(user_id=user.id, domofon_name=domofon['name'])
+                        domofon_record = models.Domofon(user_id=tenant_id, domofon_name=domofon['name'], domofon_id=domofon['id'])
                         session.add(domofon_record)
                 else:
-                    bot.send_message(user.telegram_id, 'Ошибка получения домофонов для квартиры.')
+                    return
 
             # Сохраняем все изменения в БД за один раз
             session.commit()
-            bot.send_message(user.telegram_id, 'Квартиры и домофоны успешно добавлены в базу данных.')
         else:
-            bot.send_message(user.telegram_id, 'Нет доступных квартир.')
+            return
     else:
-        bot.send_message(user.telegram_id, 'Ошибка получения списка квартир.')
+        return
 
 
 def get_tenant_id(phone_number):
@@ -97,12 +97,12 @@ def list_domofons(message):
         return
 
     # Запрашиваем домофоны, связанные с пользователем
-    domofons = session.query(models.Domofon).filter_by(user_id=user.id).all()
+    domofons = session.query(models.Domofon).filter_by(user_id=user.tenant_id).all()
 
     if domofons:
         message_text = "Список ваших домофонов:\n"
         for domofon in domofons:
-            message_text += f"id = {domofon.id}: {domofon.domofon_name}\n"
+            message_text += f"id = {domofon.domofon_id}: {domofon.domofon_name}\n"
         bot.send_message(message.chat.id, message_text)
     else:
         bot.send_message(message.chat.id, 'У вас нет домофонов в базе данных.')
@@ -110,11 +110,11 @@ def list_domofons(message):
 # Команда для получения снимка с камеры домофона
 @bot.message_handler(commands=['snapshot'])
 def get_camera_snapshot(message):
-    tenant_id = 1064
-
-    if not tenant_id:
-        bot.send_message(message.chat.id, 'Сначала авторизуйтесь с помощью /login <номер_телефона>.')
+    user = session.query(models.User).filter_by(telegram_id=message.from_user.id).first()
+    if user is None:
+        bot.send_message(message.chat.id, 'Вы не авторизованы. Пожалуйста, отправьте свой номер телефона с помощью команды /start.')
         return
+    tenant_id = user.tenant_id
 
     domofon_id = message.text.split()[1] if len(message.text.split()) > 1 else None
 
@@ -122,12 +122,23 @@ def get_camera_snapshot(message):
         bot.send_message(message.chat.id, 'Используйте: /snapshot <id_домофона>')
         return
 
+    valid_intercom = session.query(models.Domofon).filter_by(domofon_id=domofon_id, user_id=tenant_id).first()
+    if valid_intercom is None:
+        bot.send_message(message.chat.id, 'Вам не доступен этот домофон или такого домофона не сущесвтует.')
+        return
+
     payload = json.dumps({'intercoms_id' : [domofon_id], 'media_type' : ['JPEG']})
 
     response = requests.request("POST", f'{DOMOFON_API_URL}domo.domofon/urlsOnType?tenant_id={tenant_id}', headers=headers, data=payload)
 
-    if response.status_code == 200 and len(response.text) != 2:
-        snapshot_url = response.json()[0]['jpeg']
+    photo = response.json()[0]['jpeg']
+    
+    if not photo:
+        bot.send_message(message.from_user.id, 'У домофона нет камер.')
+        return
+
+    if response.status_code == 200:
+        snapshot_url = photo
         bot.send_photo(message.chat.id, photo=snapshot_url)
     else:
         error_message = 'Ошибка получения снимка.'
--- models.py
+++ models.py
@@ -7,27 +7,18 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
     phone_number = Column(String, unique=True, nullable=False)
-    tenant_id = Column(Integer, nullable=False)
+    tenant_id = Column(Integer, nullable=False, primary_key=True)
     telegram_id = Column(Integer, unique=True, nullable=False)
-    apartments = relationship("Apartment", back_populates="user")
+    
     domofons = relationship("Domofon", back_populates="user")
-
-class Apartment(Base):
-    __tablename__ = 'apartments'
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    apartment_number = Column(String)
-
-    user = relationship("User", back_populates="apartments")
 
 class Domofon(Base):
     __tablename__ = 'domofons'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    domofon_name = Column(String)
+    user_id = Column(Integer, ForeignKey('users.tenant_id'), nullable=False)
+    domofon_id = Column(Integer, nullable=False)
+    domofon_name = Column(String, nullable=False)
 
     user = relationship("User", back_populates="domofons")
